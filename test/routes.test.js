59ed85401e3c42494ac6ad496217028b53398014
harryyy27 <harryyy27@gmail.com>
2020-06-21 20:03:42 +0100

    travis fix

--- test/routes.test.js
+++ test/routes.test.js
@@ -5,9 +5,20 @@ const app = require('../src/app.js');
 tape('Tape be working', (t)=>{
     supertest(app)
     .get('/')
+    .expect(200)
     .then((response)=>{
         t.equals(response.statusCode,200)
         t.end()
     })
     
+})
+
+tape('404 route works',(t)=>{
+    supertest(app)
+    .get('/thisIsNotARoute')
+    .expect(404)
+    .then(res=> {
+        t.equals(res.statusCode,404)
+        t.end()
+    })
 })
