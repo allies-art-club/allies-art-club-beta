31144276ea1c676c97c5bb4c3996cf3c62f7a4b5
Harry Mead <harryyy27@gmail.com>
2021-02-06 16:52:07 +0000

    home page complete

---
+++ client/src/Pages/donate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {HeadingMain} from '../Components/Styled/styled'
+
+const Donate = () => {
+    return(
+        <>
+        <HeadingMain>MAIN</HeadingMain>
+        </>
+    )
+}
+export default Donate;
