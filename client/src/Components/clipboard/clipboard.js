684d9452418b381d6b3965d6e7d8587f4a260cba
Harry Mead <harryyy27@gmail.com>
2021-04-11 20:04:07 +0100

    warnings addressed

--- client/src/Components/clipboard/clipboard.js
+++ client/src/Components/clipboard/clipboard.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import { ClipboardContainer,ClipboardIcon} from '../Styled/clipboard.styled.js';
 const Clipboard = (props)=> {
     return (
