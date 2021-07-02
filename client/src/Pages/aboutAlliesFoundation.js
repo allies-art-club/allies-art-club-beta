797f419d09dba2ce9c458b256f9612a28515b110
Harry Mead <harryyy27@gmail.com>
2021-07-02 17:45:16 +0100

    changed stuff

--- client/src/Pages/aboutAlliesFoundation.js
+++ client/src/Pages/aboutAlliesFoundation.js
@@ -1,6 +1,8 @@
 import React, {Fragment} from 'react';
 import SideNav from '../Components/sideNav/sideNav.js';
 import {Section,Paragraph,Image,Link,TextImage} from '../Components/Styled/styled.js';
+import {IFrame} from '../Components/Styled/about.styled.js';
+
 import TitleBanner from '../Components/titleBanner/titleBanner.js';
 import {CentreImage} from '../Components/Styled/titleBanner.styled'
 import {BannerTextImg,ImageWrapper} from '../Components/Styled/donate.styled.js';
@@ -26,13 +28,14 @@ const AboutAlliesFoundation = () =>{
                 </ImageWrapper>
                 <Paragraph>AWTF aims to honour Allie's memory by supporting the health and wellbeing of underprivileged children, adults and families in London.</Paragraph>
                 <Paragraph>In addition to providing essentials such as food and clothing, Allie's Foundation also works to improve underprivileged people's access to education and extra-curricular activities.</Paragraph>
-                <TextImage  />
+                <TextImage alt="allie's art foundation members" src={"/assets/articles/aCharityWeCanTrust/aCharityWeCanTrust3.png"} />
                 <Paragraph>Running for over 10 years, AWTF has delivered numerous projects that have helped those in need and brightened many people's lives</Paragraph>
                 <Paragraph>Working with young people, the charity has helped provide mentorship programmes, exciting expeditions and extra-curricular activities such as art classes, sports clubs and cooking lessons.</Paragraph>
                 <Paragraph>With awareness of high levels of food poverty in London, the charity has also provided food parcels to families in need during the Summer holidays and has been running a food bank with the Pilion Trust at a North London community centre since 2017.</Paragraph>
                 <Paragraph>To learn more about AWTFs food bank, you can read our article <Link href="/discussions/project-updates/allies-food-bank">Allie's Food Bank</Link>.</Paragraph>
                 <Paragraph>With rapidly rising hunger and poverty in London due to the impacts of the Coronavirus pandemic, the charity's focus has moved mainly towards providing food and other essentials to families in need.</Paragraph>
                 <Paragraph>The following video of AWTF was made in December 2020, showing the fantastic the charity continues to do:</Paragraph>
+                <IFrame  src="https://www.youtube.com/embed/ogOJ-YFpac8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
                 <Paragraph>Allie’s Art Club was formed during the Coronavirus pandemic by Allie’s childhood friend, who was volunteering at AWTF’s food bank. Passionate about the charity’s goals of improving access to artistic pursuits and creative expression, she worked alongside other volunteers to bring the Club into fruition.</Paragraph>
                 <Paragraph>With AWTF as our foundation, we have a strong support system that enables us to do the work we love and make a difference in our community</Paragraph>
                 <Paragraph>To learn more about the founders of AWTF and why our Club trusts their foundation please view our article <Link href="/discussions/club-articles/awtf-a-charity-we-can-trust">AWTF: A Charity We Can Trust</Link>.</Paragraph>
--- client/src/Pages/aboutStory.js
+++ client/src/Pages/aboutStory.js
@@ -1,7 +1,7 @@
 import React,{Fragment} from 'react';
 import { Paragraph,Section,TextImage,InlineIcon,Link} from '../Components/Styled/styled.js';
 import SideNav from '../Components/sideNav/sideNav.js';
-import {Quote,SubTitle} from '../Components/Styled/about.styled.js';
+import {Quote,SubTitle,IFrame} from '../Components/Styled/about.styled.js';
 import {CentreImage} from '../Components/Styled/titleBanner.styled'
 
 import {BannerTextImg,ImageWrapper} from '../Components/Styled/donate.styled.js';
@@ -41,6 +41,7 @@ const AboutAlliesStory=()=>{
                 <Quote>"an extraordinary woman who truly encompassed the spirit of Wings of Hope, working with compassion, innovation and dedication to help those in need."</Quote>
                 <Paragraph>Allie loved ice-skating and spent a lot of time at Alexandra Palace skating rink in North London.</Paragraph>
                 <SubTitle>Allie's Memorial at Alexandra Palace:</SubTitle>
+                <IFrame  src="https://www.youtube.com/embed/55DNcB5R4hY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
                 <Paragraph>If you would like to know more about Allie's short yet brilliant life, you can read our article <Link href="/discussions/club-articles/remembering-allie">Remembering Allie</Link>, a collection of memories by her friends and family.</Paragraph>
                 <Paragraph>Allie's Art Club is proud to be named after such an inspiring young woman. We believe everyone deserves the opportunities that Allie had to flourish into the talented young woman so many had grown to love. We hope to honour Allie's memory in all of the work we do, continuing her lived principles of learning, kindness and fun.</Paragraph>
                 <Paragraph>Thank you Allie for all that you have inspired us to do <InlineIcon alt={"Heart"} src={"/assets/general/blackHeart.png"}/>.</Paragraph>
