099c81fdd58a259940f4806e1bcd58f5e701a934
Harry Mead <harryyy27@gmail.com>
2021-08-06 18:12:50 +0100

    title2 added to all pages

--- client/src/Pages/workSheets.js
+++ client/src/Pages/workSheets.js
@@ -25,6 +25,7 @@ const WorkSheets=()=>{
             <Article key={i} 
                 src={el.src}
                 title={el.title}
+                title2={el.title2}
                 right={i%2===1} 
                 date={el.date}
                 landingPage={"resources"}
