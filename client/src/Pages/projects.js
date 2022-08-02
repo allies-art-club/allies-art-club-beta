88790b0a18edc7afd746cf321c8a3d8286bab2cf
Harry Mead <harryyy27@gmail.com>
2022-08-02 23:13:19 +0100

    2 articles added

--- client/src/Pages/projects.js
+++ client/src/Pages/projects.js
@@ -29,7 +29,8 @@ const Projects=()=>{
             date={el.date}
             landingPage={"activities"}
             category={el.category}
-            subcategory={el.subcategory}>
+            subcategory={el.subcategory}
+            file_type={el.file_type}>
             {el.description}</Article>
                     )
                 })
