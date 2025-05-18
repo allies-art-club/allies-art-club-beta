1a2bc7883cb4447ea6bb1556b9a1392688240bd7
Ramona <ramona.florenntina@gmail.com>
2025-05-18 21:49:38 +0100

    Your commit message here

--- app/api/contactUs/route.ts
+++ app/api/contactUs/route.ts
@@ -1,50 +1,32 @@
-import { type NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
-import Mail from 'nodemailer/lib/mailer';
 
 export async function POST(request: NextRequest) {
   const { email, name, message } = await request.json();
 
   const transport = nodemailer.createTransport({
     service: 'gmail',
-    /* 
-      setting service as 'gmail' is same as providing these setings:
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true
-      If you want to use a different email provider other than gmail, you need to provide these manually.
-      Or you can go use these well known services and their settings at
-      https://github.com/nodemailer/nodemailer/blob/master/lib/well-known/services.json
-  */
     auth: {
+      type: 'OAuth2',
       user: process.env.MY_EMAIL,
-      pass: process.env.MY_PASSWORD,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
     },
   });
 
-  const mailOptions: Mail.Options = {
+  const mailOptions = {
     from: process.env.MY_EMAIL,
     to: process.env.MY_EMAIL,
-    // cc: email, (uncomment this line if you want to send a copy to the sender)
     subject: `Message from ${name} (${email})`,
     text: message,
   };
 
-  const sendMailPromise = () =>
-    new Promise<string>((resolve, reject) => {
-      transport.sendMail(mailOptions, function (err) {
-        if (!err) {
-          resolve('Email sent');
-        } else {
-          reject(err.message);
-        }
-      });
-    });
-
   try {
-    await sendMailPromise();
+    await transport.sendMail(mailOptions);
     return NextResponse.json({ message: 'Email sent' });
   } catch (err) {
-    return NextResponse.json({ error: err }, { status: 500 });
+    console.error('Email sending failed:', err);
+    return NextResponse.json({ error: err.message || 'Failed to send email' }, { status: 500 });
   }
 }
