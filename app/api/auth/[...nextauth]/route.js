94e322a2b2167336d08c9bdbf9d7e63e817b1d1d
Ramona <ramona.florenntina@gmail.com>
2025-05-18 20:59:07 +0100

    Deploy2

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,3 +5,20 @@
 // })
 
 // export {handler as GET, handler as POST}
+
+
+// app/api/auth/[...nextauth]/route.js
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github"; // or your provider(s)
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  // add more config here as needed
+});
+
+export { handler as GET, handler as POST };
